3bd9a08635731a8280a8e7c9c64d0ece3ea90590
adrian-burgos-tome <aburtom0312@g.educaand.es>
2025-12-03 17:21:51 +0100

    blur

--- js/main.js
+++ js/main.js
@@ -30,7 +30,13 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
 
-
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // Add a class if the bottom offset is greater than 50 of the viewport
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== EMAIL JS ===============*/
 
