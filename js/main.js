662b14de5b2e725db76957f4245e5437e3e071f4
adrian-burgos-tome <aburtom0312@g.educaand.es>
2025-12-03 16:59:36 +0100

    js fixed

--- js/main.js
+++ js/main.js
@@ -4,33 +4,32 @@ const navMenu = document.getElementById('nav-menu'),
       navClose = document.getElementById('nav-close')
 
 /* Menu show */
-
 if(navToggle){
-    navToggle.addEventListener('click',()=>{
+    navToggle.addEventListener('click', () =>{
         navMenu.classList.add('show-menu')
     })
-} 
+}
 
 /* Menu hidden */
-
 if(navClose){
-    navToggle.addEventListener('click',()=>{
-        navMenu.classList.add('show-menu')
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
     })
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
 const navLink = document.querySelectorAll('.nav__link')
 
 const linkAction = () =>{
     const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
 }
-navLink.forEach(n =>n.addEventListener('click', linkAction))
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== ADD BLUR HEADER ===============*/
+
 
 
 /*=============== EMAIL JS ===============*/
